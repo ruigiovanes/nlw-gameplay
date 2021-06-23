fcd27ae92cd36fb4dac17f0c525b7331ba22fc5b
Giovane Coelho <rui.ecs@interplayers.com.br>
2021-06-23 18:27:51 -0300

    - first commit

---
+++ src/components/Profile/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { styles } from './styles';
+
+import { Avatar } from '../Avatar';
+
+export function Profile() {
+  return (
+    <View style={styles.container}>
+
+      <Avatar urlImage="https://github.com/ruigiovanes.png" />
+
+      <View>
+        <View style={styles.user}>
+          <Text style={styles.greeting}>
+            Olá
+          </Text>
+
+          <Text style={styles.username}>
+            Rodrigo
+          </Text>
+        </View>
+
+        <Text style={styles.message}>
+          Hoje é dia de vitória
+        </Text>
+      </View >
+
+    </View >
+  );
+}
